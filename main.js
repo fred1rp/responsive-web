591f1e6e57704fcbbddb2aad4c983ce46b50c59b
freddy <freddy.raymundo@gmail.com>
2020-05-04 15:20:15 -0500

    app con su consola

--- main.js
+++ main.js
@@ -6,3 +6,5 @@ ScrollReveal().reveal('.showcase');
 ScrollReveal().reveal('.news-cards', {delay: 500 });
 ScrollReveal().reveal('.cards-banner-one', {delay: 500 });
 ScrollReveal().reveal('.cards-banner-two', {delay: 500 });
+
+console.log('hola me falto este mensaje');
